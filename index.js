b191944044264de338c616f80020bd73a8619b8d
lucemery <lucemery316@gmail.com>
2024-04-26 23:00:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ let pineapple = "pineapple"
 
 console.log(apple + " " + pineapple);
 
-console.log('Two plus two is ${2 + 2}')
+// console.log('Two plus two is ${2 + 2}')
+
+//console.log('The change for transaction $
+//{change}')
