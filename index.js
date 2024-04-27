3a0d72240456ef652d53c38f6cd8a2a61f6890e7
lucemery <lucemery316@gmail.com>
2024-04-26 19:31:15 -0500

    tutor session help

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+let len = "All dogs are good dogs".length;
+console.log(len)
+
+
+console.log("Apple"[3]);
+
+console.log("titanic".toUpperCase())
+
+console.log("Java".repeat(10))
+
+console.log("berry".charAt(1))
+
+console.log()
+
+let space = " ";
+
+let apple = "apple"
+
+let pineapple = "pineapple"
+
+console.log(apple + " " + pineapple);
+
+console.log('Two plus two is ${2 + 2}')
